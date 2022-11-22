c5cd242c04987a4619b97c003bf0bc98d380c851
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2022-11-22 09:28:34 +0530

    Time: 440 ms (38.02%), Space: 8.9 MB (81.88%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numSquares(int n) {
+         if (n <= 0) return 0;
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            // For each i, it must be the sum of some number (i - j*j) and 
+            // a perfect square number (j*j).
+            for (int j = 1; j*j <= i; j++)
+            {
+                dp[i] =  min(dp[i], dp[i - j*j] + 1);
+            }
+        }
+        
+        return dp[n];
+    }
+};
