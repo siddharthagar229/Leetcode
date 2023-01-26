9e1176ffb06ab4d87122d76abc09269fb00c58fd
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-01-26 13:23:52 +0530

    Time: 19 ms (41.88%), Space: 13.5 MB (22.51%) - LeetHub

--- 1286-iterator-for-combination/1286-iterator-for-combination.cpp
+++ 1286-iterator-for-combination/1286-iterator-for-combination.cpp
@@ -2,19 +2,19 @@ class CombinationIterator {
 public:
     int i=0;
     vector<string> ans;
-    void recursive(int k,vector<string> &ans,string s,int i,string main){
+    void recursive(int k,string s,int i,string main){
         if(i>main.length()) return;
         if(s.length()==k){
             ans.push_back(s);
             return;
         }
-        recursive(k,ans,s,i+1,main);
+        recursive(k,s,i+1,main);
         s.push_back(main[i]);
-        recursive(k,ans,s,i+1,main);
+        recursive(k,s,i+1,main);
     }
     CombinationIterator(string characters, int combinationLength) {
         string s="";
-        recursive(combinationLength,ans,s,0,characters);
+        recursive(combinationLength,s,0,characters);
         sort(ans.begin(),ans.end());
     }
     string next() {
