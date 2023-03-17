9c2a11fd9ce29f0c3d95ceb25444d03b1dd5a0a0
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-03-17 09:49:40 +0530

    Time: 67 ms (60.60%), Space: 45 MB (58.34%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,38 @@
+class Trie {
+    struct Node {
+        Node *child[26] = {0};
+        bool  wordend   = false;
+    };
+    Node root;
+public:
+    Trie() {}
+    
+    void insert(const string& word) {
+        Node *node = &root;
+        for (auto ch : word) {
+            ch -= 'a';
+            if (! node->child[ch])
+                node->child[ch] = new Node;
+            node = node->child[ch];
+        }
+        node->wordend = true;
+    }
+    
+    bool search(const string& word) {
+        Node *node = &root;
+        for (auto ch : word) {
+            node = node->child[ch-'a'];            
+            if (! node) return false;
+        }
+        return node->wordend;
+    }
+    
+    bool startsWith(const string& prefix) {
+        Node *node = &root;
+        for (auto ch : prefix) {
+            node = node->child[ch-'a'];            
+            if (! node) return false;
+        }
+        return true;        
+    }
+};
