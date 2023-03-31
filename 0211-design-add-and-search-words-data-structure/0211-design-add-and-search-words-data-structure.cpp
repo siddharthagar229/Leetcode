3ee27f9868b228a7e8a2cae868a9c1c6915058fd
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-03-31 10:04:04 +0530

    Time: 1963 ms (20.97%), Space: 136.9 MB (90.52%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,32 @@
+class WordDictionary {
+private:
+    bool check(string &a, string &b){
+        int i = 0;
+        while(i < a.size()){
+            if(b[i] != '.' && b[i] != a[i]) return false;
+            i++;
+        }
+
+        return true;
+    }
+
+public:
+    unordered_map<int, vector<string>> mp;
+
+    WordDictionary() {}
+    
+    void addWord(string word) {
+        mp[word.size()].push_back(word);
+    }
+    
+    bool search(string word) {
+        if(mp.find(word.size()) == mp.end()) return false;
+
+        for(auto &i : mp[word.size()]){
+            bool ans = check(i, word);
+            if(ans) return true;
+        }
+
+        return false;
+    }
+};
