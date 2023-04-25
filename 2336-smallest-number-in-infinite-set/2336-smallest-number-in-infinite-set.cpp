d3a33cf4b5f20d2385fccb5fa417afafe54827da
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-04-25 09:31:58 +0530

    Time: 126 ms (36.75%), Space: 47.6 MB (7.32%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -0,0 +1,22 @@
+class SmallestInfiniteSet {
+public:
+    set<int> s;
+    SmallestInfiniteSet() {
+        constexpr int N = 1000;
+        for (int i = 1; i <= N; ++i) {
+          s.emplace(i);
+        }
+    }
+
+    int popSmallest() {
+        const int res = *s.begin();
+        s.erase(s.begin());
+        return res;
+    }
+
+    void addBack(int num) {
+        s.emplace(num);
+    }
+
+
+};
