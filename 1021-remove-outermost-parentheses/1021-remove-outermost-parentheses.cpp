acbb2116cc804fb288bfc7b7f886ffc6945cb2cf
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-08-03 11:30:51 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (39.02%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
 
-string removeOuterParentheses(string S) {
+string removeOuterParentheses(string s) {
 
-     int st=0;
-    string ans;
-    for(auto a:S)
-    {
-        if(a=='(')
-        {
-            if(st>0)
-            {
-                ans+='(';
+// to store the ans
+        string ans = "";
+        // to keep the track of valid parenthesis we have initialized count variable
+        // valid parenthesis contains equal number of 
+        int count = 0;
+        for(char ch: s){
+            if(ch == '(' && count == 0){
+                // this bracket is a part of outermost parenthesis
+                // dont add it to the ans, skip it
+                count++;
+            }else if(ch == '(' && count >= 1){
+                // this bracket is a part of inner parenthesis
+                // add it to the ans
+                ans += ch;
+                count++;
+                
+            }else if(ch == ')' && count > 1){
+                // this bracket is a part of inner parenthesis
+                // add it to the ans, and decrease the count
+                ans += ch;
+                count--;
+            }else if(ch == ')' && count == 1){
+                // this bracket is a part of outer parenthesis
+                // avoid it
+                count--;
             }
-            st++;
         }
-        else
-        {
-            if(st>1)
-            {
-                ans+=')';
-            }
-            st--;
-        }
-    }
-    return ans;
+        
+        return ans;
+
 }
 };
