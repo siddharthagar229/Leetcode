c5d6d56f84d2644a24dce0244847237d2c580e4e
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-08-12 16:24:47 +0530

    Time: 338 ms (93.46%), Space: 89.7 MB (52.23%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,38 +2,18 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         
-        sort(points.begin(),points.end());
-
-//start is the start of first balloon
-        int start = points[0][0];
-		
-//end is the end of first balloon
-        int end = points[0][1];
-		
-// we would need atleast 1 arrow to burst the first balloon
-        int ans = 1;
-		
-//start a for loop for other balloons
-        for(int i=1;i<points.size();i++){
-		
-//if start of any balloon is before end of the balloon, it means they coincide
-                if(points[i][0]<=end){
-				
-// since we need to find the intersection, find the start value more to the right
-                    start = max(start,points[i][0]);
-					
-// since we need to find the intersection, find the end value more to the left
-                    end = min(end,points[i][1]);
-                }else{
-				
-//else we know that the current balloon doesnt coincide with other balloons 
-//so we set start and end to the current balloon and increase the ans by one
-                    start = points[i][0];
-                    end = points[i][1];
-                    ans++;
-                }
+         sort(points.begin(),points.end());
+        int first,second,i=0,n=points.size(),cnt=0;
+        while(i<n){
+            first=points[i][0],second=points[i][1];
+            while(i<n && points[i][0]<=second && points[i][0]>=first){
+                second=min(second,points[i][1]);
+                first=max(first,points[i][0]);
+                i++;
+            }
+            cnt++;
         }
-        return ans;
+        return cnt;
         
     }
 };
