985a3608f158cc3cc59dbcbb329d81069240ac1a
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-09-18 11:34:41 +0530

    Time: 18 ms (38.37%), Space: 23.1 MB (94.29%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div508-most-frequent-subtree-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divapple-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div508-most-frequent-subtree-sum.cpp
@@ -0,0 +1,49 @@
+
+class Solution {
+public:
+            int solve(TreeNode *root , vector<int> &ans){
+            if(!root) return 0;
+
+            int left= solve(root->left , ans);
+            int right= solve(root->right , ans);
+            int temp=left + right + root->val;
+            ans.push_back(temp);
+
+            return temp; 
+
+        }
+
+
+        vector<int> findFrequentTreeSum(TreeNode* root) {
+
+            vector<int> ans;
+            solve(root , ans);                          
+            int maxi=INT_MIN;
+            map<int,int> mp;
+
+            for(auto i:ans){
+                mp[i]++;                                
+            }
+
+            for(auto i:mp){
+                if(maxi<i.second){
+                    maxi=i.second;                      
+                }
+            }
+
+            if(maxi==1){                     
+                return ans;                 
+            }
+
+            else{
+                vector<int>ds;
+                 for(auto i:mp){
+                    if(maxi==i.second){                              
+                        ds.push_back(i.first);                        
+                    }
+                }
+                return ds;                     
+            }
+
+        }
+};
