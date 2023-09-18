702e71da034fe0f9f6e0b3e167fbdb4e5d6fc105
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-09-18 22:13:12 +0530

    Time: 585 ms (24.53%), Space: 289.7 MB (10.86%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1519-number-of-nodes-in-the-sub-tree-with-the-same-label/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1519-number-of-nodes-in-the-sub-tree-with-the-same-label/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1519-number-of-nodes-in-the-sub-tree-with-the-same-label.cpp
@@ -1,35 +1,34 @@
 class Solution {
-    vector<int> array;
 public:
-    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
-        array.resize(n); //output array
-        
-        //creating adjacency list
-        vector<vector<int>> a(n);
-        for(auto x:edges){
-            a[x[0]].push_back(x[1]);
-            a[x[1]].push_back(x[0]);
-        }
-        dfs(-1,0,a,labels);
-        return array;
-    }
+    vector<int>dfs(vector<vector<int>>&adj,int curr,int parent,vector<int>&result,string &labels){
+        vector<int>my_count(26,0);
+        int mylabel=labels[curr];
+        my_count[mylabel- 'a']=1;
 
-    vector<int> dfs(int prev,int curr,vector<vector<int>>& a,string& labels)
-    {
-        vector<int> ans(26); 
-        for(auto x:a[curr])
-        {
-            if(prev!=x)
-            {
-            // array return by the children node
-                vector<int> res = dfs(curr,x,a,labels); 
-            // combining the frequencies of left and right subtrees into one array
-                for(int i=0;i<26;i++) 
-                    ans[i]+=res[i];
+        for(auto& child:adj[curr]){
+            if(child==parent) continue;
+            vector<int>child_count(26,0);
+            child_count=dfs(adj,child,curr,result,labels);
+
+            for(int i=0;i<26;i++){
+                my_count[i]+=child_count[i];
             }
         }
-        // incrementing the freq of curr node label and storing in output array
-        array[curr] = ++ans[labels[curr]-'a'];
-        return ans;
+        result[curr]=my_count[mylabel-'a'];
+        return my_count;
+    }
+    vector<int> countSubTrees(int n, vector<vector<int>>& edges, string labels) {
+        vector<vector<int>> adj(n);
+        for(auto& it: edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+
+        }
+        vector<int>result(n,0);
+
+        dfs(adj,0,-1,result,labels);
+        return result;
     }
 };
