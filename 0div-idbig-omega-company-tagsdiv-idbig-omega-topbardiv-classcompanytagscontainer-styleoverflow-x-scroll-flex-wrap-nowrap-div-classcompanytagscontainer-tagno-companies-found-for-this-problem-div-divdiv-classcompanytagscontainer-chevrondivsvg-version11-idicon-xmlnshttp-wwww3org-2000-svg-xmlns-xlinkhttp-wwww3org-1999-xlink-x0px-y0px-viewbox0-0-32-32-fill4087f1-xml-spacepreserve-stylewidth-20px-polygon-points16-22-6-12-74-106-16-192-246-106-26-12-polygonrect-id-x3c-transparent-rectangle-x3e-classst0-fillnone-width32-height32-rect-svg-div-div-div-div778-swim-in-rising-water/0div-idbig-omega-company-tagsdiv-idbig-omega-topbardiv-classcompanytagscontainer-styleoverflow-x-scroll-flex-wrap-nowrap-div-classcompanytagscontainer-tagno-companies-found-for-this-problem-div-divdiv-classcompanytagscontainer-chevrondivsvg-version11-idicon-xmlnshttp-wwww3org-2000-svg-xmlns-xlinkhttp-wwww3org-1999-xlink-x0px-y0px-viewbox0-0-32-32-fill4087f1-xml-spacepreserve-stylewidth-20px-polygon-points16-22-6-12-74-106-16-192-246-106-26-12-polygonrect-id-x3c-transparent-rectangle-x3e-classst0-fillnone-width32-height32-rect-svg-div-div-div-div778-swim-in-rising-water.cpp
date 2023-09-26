c7aa137d6bf62360fd2d18c85bc4c9a14a5bcc60
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-09-26 15:50:25 +0530

    Time: 15 ms (63.29%), Space: 8.9 MB (73.83%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div778-swim-in-rising-water.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        
+        
+        
+       priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>>pq;
+        int n = grid.size(), m = grid[0].size();
+        int dirx[4] = {1, -1, 0, 0};
+        int diry[4] = {0, 0, -1, 1};
+        int val = 0;
+        pq.push({grid[0][0],{0,0}});
+        grid[0][0] = -1;
+        while(!pq.empty())
+        {
+            int sz = pq.size();
+            while(sz--)
+            {
+                pair<int, pair<int, int>> tp = pq.top();
+                int x = tp.second.first, y = tp.second.second;
+                pq.pop();
+                val = max(val, tp.first);
+                if(x == n-1 && y == m-1) return val;
+                for(int i = 0; i < 4; i++)
+                {
+                    int nxtx = x + dirx[i];
+                    int nxty = y + diry[i];
+                    if(nxtx < 0 || nxtx >= n || nxty < 0 || nxty >= m) continue;
+                    if(grid[nxtx][nxty] == -1) continue;
+                    pq.push({grid[nxtx][nxty],{nxtx, nxty}});
+                    grid[nxtx][nxty] = -1;
+                }
+            }
+        }
+        return val;
+        
+        
+    }
+};
