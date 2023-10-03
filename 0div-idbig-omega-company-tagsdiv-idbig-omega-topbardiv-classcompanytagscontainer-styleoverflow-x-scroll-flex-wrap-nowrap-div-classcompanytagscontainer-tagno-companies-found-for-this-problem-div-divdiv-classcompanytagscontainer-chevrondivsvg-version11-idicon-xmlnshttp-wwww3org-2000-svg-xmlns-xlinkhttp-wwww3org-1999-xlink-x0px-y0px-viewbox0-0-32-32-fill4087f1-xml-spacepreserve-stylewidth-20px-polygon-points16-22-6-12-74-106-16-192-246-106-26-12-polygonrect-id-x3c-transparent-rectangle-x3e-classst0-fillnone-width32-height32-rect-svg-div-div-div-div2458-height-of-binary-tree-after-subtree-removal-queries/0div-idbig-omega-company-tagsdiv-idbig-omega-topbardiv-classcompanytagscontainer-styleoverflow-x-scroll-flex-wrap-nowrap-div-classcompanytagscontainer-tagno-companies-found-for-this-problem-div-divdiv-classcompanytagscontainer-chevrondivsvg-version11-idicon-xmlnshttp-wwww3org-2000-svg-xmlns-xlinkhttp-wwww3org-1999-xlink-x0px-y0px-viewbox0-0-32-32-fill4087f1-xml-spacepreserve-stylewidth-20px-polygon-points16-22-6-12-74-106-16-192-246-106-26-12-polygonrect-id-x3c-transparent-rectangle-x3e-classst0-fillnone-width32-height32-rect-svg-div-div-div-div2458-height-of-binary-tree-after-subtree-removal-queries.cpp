57f4190220c81e11091cb3fc9477a834603741ec
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-10-03 12:50:55 +0530

    Time: 1075 ms (10.17%), Space: 422.1 MB (10.86%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2458-height-of-binary-tree-after-subtree-removal-queries/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    map<int,int> l,r,h;
+    int height(TreeNode *root)
+    {
+        if(!root) return 0;
+        
+        int lh = height(root->left);
+        int rh = height(root->right);
+        
+        l[root->val] = lh;
+        r[root->val] = rh;
+        
+        return max(lh,rh)+1;
+    }
+    void solve(TreeNode *root,int maxa,int depth){
+        if(!root) return;
+        
+        h[root->val] = maxa;
+        
+        solve(root->left, max(maxa, depth+r[root->val]), depth+1);
+        solve(root->right, max(maxa, depth+l[root->val]),depth+1);
+    }
+    vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
+        height(root);
+        solve(root->left, r[root->val], 1);
+        solve(root->right, l[root->val], 1);
+        
+        vector<int> ans;
+        
+        for(int i=0;i<queries.size();i++){
+            ans.push_back(h[queries[i]]);
+        }
+        
+        return ans;
+    }
+};
