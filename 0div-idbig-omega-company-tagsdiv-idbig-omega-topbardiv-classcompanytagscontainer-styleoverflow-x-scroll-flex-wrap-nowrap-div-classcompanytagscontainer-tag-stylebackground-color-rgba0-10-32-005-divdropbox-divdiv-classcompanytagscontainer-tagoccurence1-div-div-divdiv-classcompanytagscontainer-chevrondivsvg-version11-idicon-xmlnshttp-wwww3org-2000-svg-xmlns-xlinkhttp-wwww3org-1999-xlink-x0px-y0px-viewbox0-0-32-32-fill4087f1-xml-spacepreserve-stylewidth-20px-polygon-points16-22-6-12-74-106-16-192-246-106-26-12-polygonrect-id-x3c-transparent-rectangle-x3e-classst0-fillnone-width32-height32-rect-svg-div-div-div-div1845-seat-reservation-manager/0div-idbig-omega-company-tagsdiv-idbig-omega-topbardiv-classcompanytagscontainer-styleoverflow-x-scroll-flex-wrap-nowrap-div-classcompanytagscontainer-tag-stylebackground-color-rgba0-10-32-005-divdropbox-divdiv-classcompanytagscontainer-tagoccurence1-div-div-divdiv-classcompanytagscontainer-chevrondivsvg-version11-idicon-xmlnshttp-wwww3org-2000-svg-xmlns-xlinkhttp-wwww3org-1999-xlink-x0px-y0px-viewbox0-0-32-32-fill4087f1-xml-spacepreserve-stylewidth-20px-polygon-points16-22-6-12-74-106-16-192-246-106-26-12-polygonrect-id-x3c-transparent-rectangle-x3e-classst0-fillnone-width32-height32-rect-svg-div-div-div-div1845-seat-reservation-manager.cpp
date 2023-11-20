9a95d6d6587a565d8c2b86a6062758dd590bc47d
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2023-11-20 12:01:51 +0530

    Time: 300 ms (51.62%), Space: 148.2 MB (35.11%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdropbox-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1845-seat-reservation-manager/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divdropbox-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1845-seat-reservation-manager.cpp
@@ -0,0 +1,23 @@
+class SeatManager {
+public:
+    // time: O(nlogn)
+    SeatManager(int n) {
+        for (int i = 1; i <= n; i++) unreserved_seats.push(i);
+    }
+    
+    // time: O(logn)
+    int reserve() {
+        if (unreserved_seats.empty()) return NULL;
+        int seat = unreserved_seats.top();
+        unreserved_seats.pop();
+        return seat;
+    }
+    
+    // time: O(logn)
+    void unreserve(int seatNumber) {
+        unreserved_seats.push(seatNumber);
+    }
+private:
+    // space: O(n)
+    priority_queue<int, vector<int>, greater<int>> unreserved_seats;
+};
