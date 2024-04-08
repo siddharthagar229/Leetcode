0bcde701c679c82f6489604a59995dfd1d079ff8
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2024-04-08 06:01:50 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (95.64%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divflipkart-divdiv-classcompanytagscontainer-tagoccurence7-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        
-         queue<int>q;
-        for(auto s:students){
-            q.push(s);
-        }
-        int i=0, c=0;
-        while(i<sandwiches.size()){
-            if(sandwiches[i]==q.front()){
-                q.pop();
-                i++;
-            } else {
-                int x=q.front();
-                q.pop();
-                q.push(x);
-            }
-            c++;
-            if(c==10001)
-                break;
-        }
-        return q.size();
+       int student_count = students.size(), circular_stu=0, square_stu=0;
+	for(int i=0; i<students.size(); i++){
+		if(students[i] == 0){circular_stu++;}
+		else{square_stu++;}
+	}
+	for(int i=0; i<sandwiches.size(); i++){
+		if(sandwiches[i] == 0){
+			if(circular_stu > 0){
+				student_count--;  // because he'll take it and leave the line.
+				circular_stu--;    // circular student is also gone.
+			}
+			else{return student_count;}
+		}
+		else{
+			if(square_stu > 0){
+				student_count--;
+				square_stu--;
+			}
+			else{return student_count;}
+		}
+	}
+	return student_count;
         
     }
 };
