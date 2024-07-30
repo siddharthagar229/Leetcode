dd61b891d857be21e8ee0792469eb60d58b154eb
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2024-07-30 19:36:51 +0530

    Time: 74 ms (93.30%), Space: 23.6 MB (73.37%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1653-minimum-deletions-to-make-string-balanced/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        
+         int ans=0;
+        int countB=0;
+        for(char c:s){
+            if(c=='b'){
+                countB++;
+            }else{
+                ans=min(ans+1,countB);
+            }
+        }
+        return ans;
+        
+    }
+};
