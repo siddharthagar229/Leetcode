7defd7794a57b6d557573108bcc540ed78c5cff8
Siddharth Agrawal <62170263+siddharthagar229@users.noreply.github.com>
2024-12-14 18:30:44 +0530

    Time: 79 ms (93.11%), Space: 111.9 MB (91.88%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2762-continuous-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2762-continuous-subarrays.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  #define ll long long
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        int range_min=INT_MAX;
+        int range_max=INT_MIN;
+
+        ll count=0;
+        ll win_size;
+        int right;
+        for(right=0;right<n;++right){
+            range_min = min(range_min,nums[right]);
+            range_max = max(range_max,nums[right]);
+
+            if(range_max-range_min > 2){
+                win_size = right-left;
+                count += (win_size*(win_size+1))/2;
+
+                left = right;
+                //Expand current window to as left as possible
+                range_min = nums[right];
+                range_max = nums[right];
+                while(abs(nums[right]-nums[left-1])<=2){
+                    left--;
+                    range_min = min(range_min,nums[left]);
+                    range_max = max(range_max,nums[left]);
+                }
+                //Subtract overcounted subarrays
+                if(left < right){
+                    win_size = right-left;
+                    count -= (win_size*(win_size+1))/2;
+                }
+            }
+        }
+        //Add subarrays from last window
+        win_size = right-left;
+        count += (win_size*(win_size+1))/2;
+        return count;
+
+    }
+};
